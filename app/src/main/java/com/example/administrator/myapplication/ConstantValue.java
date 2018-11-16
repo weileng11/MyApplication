73fa8b7d106dcddf84be760d7133e63f934a3d2b
weileng143@163.com <litao!143>
2018-11-16 13:19:01 +0800

    PhotoView+Glide+Dialog+ViewPager:打造轻量级的图片浏览方案

---
+++ app/src/main/java/com/example/administrator/myapplication/ConstantValue.java
@@ -0,0 +1,12 @@
+package com.example.administrator.myapplication;
+
+/**
+ * Created by xiaoke on 2016/6/28.
+ */
+public class ConstantValue {
+    public static final String PIC_URL1="http://img4.duitang.com/uploads/item/201312/05/20131205171756_cfAFz.thumb.600_0.jpeg";
+    public static final String PIC_URL2="http://cdn.duitang.com/uploads/item/201312/05/20131205171755_CU5Uc.jpeg";
+    public static final String PIC_URL3="http://v1.qzone.cc/skin/201511/28/10/38/565913ac38a5c105.jpg%21600x600.jpg";
+    public static final String PIC_URL4="http://img4.duitang.com/uploads/item/201312/05/20131205171759_jvyaZ.jpeg";
+
+}
--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -5,34 +5,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  @author ${bruce} */
 public class MainActivity extends AppCompatActivity
 {
+	private List<String> list;
+	private String[] urls={ConstantValue.PIC_URL1,ConstantValue.PIC_URL2,ConstantValue.PIC_URL3,ConstantValue.PIC_URL4};
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//获取当前设备的屏幕密度等基本参数 ShowImagesDialog
 		getDeviceDensity();
-		final ArrayList<String> urls=new ArrayList<>();
-		urls.add(
-				"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511198824138&di=cec97b6363a1bce28b8499a31b78df83&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farchive%2F3e282f8762696b0bbb3ed16a5dc193c718e5aff9.jpg");
-		urls.add(
-				"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511793592&di=08b8de22336028a68c9a0bbbe7a9066d&imgtype=jpg&er=1&src=http%3A%2F%2Fi1.hdslb.com%2Fbfs%2Farchive%2F7ea7878cfbddb27bb8a23e2407bfa7a48655f317.jpg");
-		urls.add(
-				"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511198824136&di=f4dc71ffdbbb16d9e3d496cf8add5376&imgtype=0&src=http%3A%2F%2Foss.tan8.com%2Fresource%2Fattachment%2F2017%2F201707%2F962b7304d0adc7e2e1d374dc6786e302.jpg");
-		urls.add(
-				"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511793592&di=08b8de22336028a68c9a0bbbe7a9066d&imgtype=jpg&er=1&src=http%3A%2F%2Fi1.hdslb.com%2Fbfs%2Farchive%2F7ea7878cfbddb27bb8a23e2407bfa7a48655f317.jpg");
-		urls.add(
-				"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511198824136&di=f4dc71ffdbbb16d9e3d496cf8add5376&imgtype=0&src=http%3A%2F%2Foss.tan8.com%2Fresource%2Fattachment%2F2017%2F201707%2F962b7304d0adc7e2e1d374dc6786e302.jpg");
+		
+		initData();
+		//final ArrayList<String> urls=new ArrayList<>();
+		//urls.add(
+		//		"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511198824138&di=cec97b6363a1bce28b8499a31b78df83&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farchive%2F3e282f8762696b0bbb3ed16a5dc193c718e5aff9.jpg");
+		//urls.add(
+		//		"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511793592&di=08b8de22336028a68c9a0bbbe7a9066d&imgtype=jpg&er=1&src=http%3A%2F%2Fi1.hdslb.com%2Fbfs%2Farchive%2F7ea7878cfbddb27bb8a23e2407bfa7a48655f317.jpg");
+		//urls.add(
+		//		"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511198824136&di=f4dc71ffdbbb16d9e3d496cf8add5376&imgtype=0&src=http%3A%2F%2Foss.tan8.com%2Fresource%2Fattachment%2F2017%2F201707%2F962b7304d0adc7e2e1d374dc6786e302.jpg");
+		//urls.add(
+		//		"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511793592&di=08b8de22336028a68c9a0bbbe7a9066d&imgtype=jpg&er=1&src=http%3A%2F%2Fi1.hdslb.com%2Fbfs%2Farchive%2F7ea7878cfbddb27bb8a23e2407bfa7a48655f317.jpg");
+		//urls.add(
+		//		"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511198824136&di=f4dc71ffdbbb16d9e3d496cf8add5376&imgtype=0&src=http%3A%2F%2Foss.tan8.com%2Fresource%2Fattachment%2F2017%2F201707%2F962b7304d0adc7e2e1d374dc6786e302.jpg");
 		findViewById(R.id.txv_click).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View v){
-				new ShowImagesDialog(MainActivity.this, urls,0).show();
+				new ShowImagesDialog(MainActivity.this, list,0).show();
 			}
 		});
+	}
+	
+	private void initData() {
+		list=new ArrayList<>();
+		for (int i=0;i<urls.length;i++){
+			list.add(urls[i]);
+		}
 	}
 	
 	/**
